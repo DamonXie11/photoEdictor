3d4e40eb739578366452f5b269123dac35521cca
DamonXie11 <1350832170@qq.com>
2021-04-13 19:15:00 +0800

    4.13

--- src/components/imgDraw/toBase64.js
+++
@@ -1,7 +0,0 @@
-var canvas = document.createElement("canvas");
-canvas.width = img.width;
-canvas.height = img.height;
-var ctx = canvas.getContext("2d");
-ctx.drawImage(img, 0, 0, img.width, img.height);
-var ext = img.src.substring(img.src.lastIndexOf(".")+1).toLowerCase();
-var dataURL = canvas.toDataURL("image/"+ext);
